b39c329d95a89390e5d5667afd5addf64afffac0
yuyizhong <zoe_yyz@hotmail.com>
2022-12-17 22:50:26 +0000

    Add js code to call game menu when user clicking on game guide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+// Display game menu when user clicking on 'Game Guide' button
+let guideModal = document.getElementById("guide-modal");
+let guideBtn = document.getElementById("guide-btn");
+let close = document.getElementsByClassName("close")[0];
 
+function openGuide(){
+    guideModal.style.display = "block";
+}
+guideBtn.addEventListener("click", openGuide);
