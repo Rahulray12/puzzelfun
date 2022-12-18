86bb89a5b25400063f6a1b8d3c997749fcade506
yuyizhong <zoe_yyz@hotmail.com>
2022-12-18 00:06:09 +0000

    ae menu style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-// Display game menu when user clicking on 'Game Guide' button
+// Display game guide when user clicking on 'Game Guide' button
+
 let guideModal = document.getElementById("guide-modal");
 let guideBtn = document.getElementById("guide-btn");
 let close = document.getElementsByClassName("close")[0];
-
-function openGuide(){
+function openGuide() {
     guideModal.style.display = "block";
 }
 guideBtn.addEventListener("click", openGuide);
+
+// Close the game guide when clicking on (x)
+
+function closeGuide() {
+    guideModal.style.display = "none";
+}
+close.addEventListener("click", closeGuide);
