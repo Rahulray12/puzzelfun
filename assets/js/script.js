676668f50727f99af2da1d98d6b991171f48eeef
yuyizhong <zoe_yyz@hotmail.com>
2022-12-20 22:26:32 +0000

    set up creattable function and displaypuzzle functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", function() {
 			if (this.getAttribute("data-type") === "guide") {
 				openGuide();
 			} else if (this.getAttribute("data-type") === "start") {
-                    shuffle(); 
+                shuffle(); 
             } else if (this.getAttribute("data-type") === "pause") {
-                        pause(); 
+                pause(); 
             } else {let gameType = this.getAttribute("data-type");
 				runGame(gameType);
 			}
@@ -23,91 +23,91 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("guide-modal").style.display = "none";
     });
 
-    // Creat table
-    let Type;
-    let numArray = [];
+      
+       
+    // Display game guide when user clicking on 'Game Guide' button
+    function openGuide() {
+    let guideModal = document.getElementById("guide-modal");
+    let guideBtn = document.getElementById("guide-btn");
+    guideModal.style.display = "block";
+    }
 
-    function createTable() {
+    function runGame(gameType) {
+
+        if (gameType === "3") {
+            display3puzzle();
+        } else if (gameType === "4") {
+            display4puzzle();
+        } else if (gameType === "5") {
+            display5puzzle();
+        } else {
+            alert(`Game type ${gameType} not found`);
+            throw `Erro game type ${gameType}, aborting!`;
+        }
+
+    }
+      
+
+    // need to think how to trigger checkwin
+
+    // Default game is 3X3 puzzle when DOM is loaded
+    runGame("3");
+    
+});
+
+// creat table
+function createTable(type) {
+    let numArray=[];
     const myTable = document.createElement("table");
     myTable.id = "myTable";
-    document.getElementById(puzzel).innerHTML=myTable;
+    document.getElementById("puzzle").appendChild(myTable);
+    
 
-    let Count = 0;
-    for (let i = 0; i < Type; i++) {
+    let Count = 0; 
+    for (let i = 0; i < type; i++) {
         const row = document.createElement("tr");
-        for (j = 0; j < Type; j++) {
+        for (j = 0; j < type; j++) {
             const tile = document.createElement("td");
             tile.id = Count;
             tile.addEventListener("click", moveTile);
             row.appendChild(tile);
             Count++;
+            
         }
         myTable.appendChild(row);
     }
 
-    for (let i = 0; i < Type * Type; i++) {
+    for (let i = 0; i < type * type; i++) {
         let tile = document.getElementsByTagName("td");
         if (numArray[i] == type * type) {
             tile.innerHTML = "";
-            tile.className = "empty";
+            tile.id = "empty";
             } else {
-            tile.innerHTML = numbers[i];
+            tile.innerHTML = numArray[i];
             tile.className = "tile";
             }
         }      
-    }
-
-    // display3x3puzzel
-    function display3puzzel () {
-        let type=3;
-        createTable ();
-    }
-
-    // display4x4puzzel
-    function display4puzzel () {
-        let type=4;
-        createTable ();
-    }
-
-    // display4x4puzzel
-    function display5puzzel () {
-        let type=5;
-        createTable ();
-    }
-
-    // need to think how to trigger checkwin
-
-    // Default game is 3X3 puzzel when DOM is loaded
-    runGame("3");
-    
-});
-
-
-
-
-// Display game guide when user clicking on 'Game Guide' button
-function openGuide() {
-    let guideModal = document.getElementById("guide-modal");
-    let guideBtn = document.getElementById("guide-btn");
-    guideModal.style.display = "block";
+        
 }
 
-
-function runGame(gameType) {
-
-    if (gameType === "3") {
-		display3puzzel;
-	} else if (gameType === "4") {
-		display4puzzel;
-	} else if (gameType === "5") {
-		display5puzzel;
-	} else {
-		alert(`Game type ${gameType} not found`);
-		throw `Erro game type ${gameType}, aborting!`;
-	}
-
-
+// display3x3puzzle
+function display3puzzle() {
+    let type=3;
+    createTable(type);
 }
+
+// display4x4puzzle
+function display4puzzle() {
+    let type=4;
+    createTable(type);
+}
+
+// display4x4puzzle
+function display5puzzle() {
+    let type=5;
+    createTable(type);
+}
+
 function shuffle() {
 
 }
@@ -131,14 +131,5 @@ function Checkwin() {
 function ncrementalScore() {
 
 }
-function display3puzzel() {
-    
 
-}
-function display4puzzel() {
-
-}
-function display5puzzel() {
-    
-}
 
