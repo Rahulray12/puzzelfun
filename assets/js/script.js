2e7ca1fe4b94295551b7ced615977d57ebee494a
yuyizhong <zoe_yyz@hotmail.com>
2022-12-20 18:07:55 +0000

    Creat table to display puzzels

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,63 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("guide-modal").style.display = "none";
     });
 
+    // Creat table
+    let Type;
+    let numArray = [];
+
+    function createTable() {
+    const myTable = document.createElement("table");
+    myTable.id = "myTable";
+    document.getElementById(puzzel).innerHTML=myTable;
+
+    let Count = 0;
+    for (let i = 0; i < Type; i++) {
+        const row = document.createElement("tr");
+        for (j = 0; j < Type; j++) {
+            const tile = document.createElement("td");
+            tile.id = Count;
+            tile.addEventListener("click", moveTile);
+            row.appendChild(tile);
+            Count++;
+        }
+        myTable.appendChild(row);
+    }
+
+    for (let i = 0; i < Type * Type; i++) {
+        let tile = document.getElementsByTagName("td");
+        if (numArray[i] == type * type) {
+            tile.innerHTML = "";
+            tile.className = "empty";
+            } else {
+            tile.innerHTML = numbers[i];
+            tile.className = "tile";
+            }
+        }      
+    }
+
+    // display3x3puzzel
+    function display3puzzel () {
+        let type=3;
+        createTable ();
+    }
+
+    // display4x4puzzel
+    function display4puzzel () {
+        let type=4;
+        createTable ();
+    }
+
+    // display4x4puzzel
+    function display5puzzel () {
+        let type=5;
+        createTable ();
+    }
+
     // need to think how to trigger checkwin
 
     // Default game is 3X3 puzzel when DOM is loaded
     runGame("3");
-
+    
 });
 
 
@@ -42,6 +94,18 @@ function openGuide() {
 
 
 function runGame(gameType) {
+
+    if (gameType === "3") {
+		display3puzzel;
+	} else if (gameType === "4") {
+		display4puzzel;
+	} else if (gameType === "5") {
+		display5puzzel;
+	} else {
+		alert(`Game type ${gameType} not found`);
+		throw `Erro game type ${gameType}, aborting!`;
+	}
+
 
 }
 function shuffle() {
